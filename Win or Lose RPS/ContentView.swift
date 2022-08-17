62ee05d90e62226093afda738201b72ee212b248
Zhich <nick.zhicheng@gmail.com>
2022-08-16 22:44:25 -0300

    Initial Commit

---
+++ Win or Lose RPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Win or Lose RPS
+//
+//  Created by Nick on 2022-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Win or Lose RPS/Win_or_Lose_RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Win_or_Lose_RPSApp.swift
+//  Win or Lose RPS
+//
+//  Created by Nick on 2022-08-16.
+//
+
+import SwiftUI
+
+@main
+struct Win_or_Lose_RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
