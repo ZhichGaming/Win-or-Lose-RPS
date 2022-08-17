4243d64dc11bf34e7da0deec912ca3c21760feec
ZhichGaming <nick.zhicheng@gmail.com>
2022-08-17 18:37:25 -0300

    Added title, rules and basic variables

--- Win or Lose RPS/ContentView.swift
+++ Win or Lose RPS/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+let moves: [String] = ["Rock", "Paper", "Scissors"]
+
 struct ContentView: View {
+    
+    @State var currentChoice: String = moves[Int.random(in: 0..<3)]
+    @State var shouldWin: Bool = Bool.random()
+    @State var playerScore: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Win or lose RPS")
+                .font(.largeTitle)
+                .padding(.top)
+            Text("Standard rock paper scissors rules but you win by winning or losing according to below")
+                .padding(.horizontal)
+                .multilineTextAlignment(.center)
+            // Playerscore
+            
+            // App's move
+            
+            // Win or lose
+            
+            Spacer()
+        }
     }
 }
 
